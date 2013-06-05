6e3d64d54f59f3579776b06ab5dd67d6cb7d4691
Joseph Abrahamson <me@jspha.com>
2013-06-05 00:38:35 -0400

    Initial commit

---
+++ src/Crypto/Salty/ByteSizes.hs
@@ -0,0 +1,312 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Crypto.Salty.ByteSizes (
+
+  auth,
+  authKey,
+  boxPK,
+  boxSK,
+  boxNonce,
+  boxZero,
+  boxBoxZero,
+  boxMac,
+  boxBeforeNM,
+  onetime,
+  onetimeKey,
+  scalarMult,
+  scalarMultScalar,
+  secretBoxKey,
+  secretBoxNonce,
+  secretBoxZero,
+  secretBoxBoxZero,
+  sign,
+  signPK,
+  signSK
+
+  ) where
+
+import Foreign.C
+
+-- Constants for
+
+auth, authKey, boxPK, boxSK, boxNonce, boxZero, boxBoxZero, boxMac :: Int
+boxBeforeNM, onetime, onetimeKey, scalarMult, scalarMultScalar :: Int
+secretBoxKey, secretBoxNonce, secretBoxZero, secretBoxBoxZero :: Int
+sign, signPK, signSK :: Int
+
+
+-- Authentication
+auth    = fromIntegral c_crypto_auth_bytes    :: Int
+authKey = fromIntegral c_crypto_auth_keybytes :: Int
+
+-- Box
+boxPK       = fromIntegral c_crypto_box_publickeybytes :: Int
+boxSK       = fromIntegral c_crypto_box_secretkeybytes :: Int
+boxNonce    = fromIntegral c_crypto_box_noncebytes     :: Int
+boxZero     = fromIntegral c_crypto_box_zerobytes      :: Int
+boxBoxZero  = fromIntegral c_crypto_box_boxzerobytes   :: Int
+boxMac      = fromIntegral c_crypto_box_macbytes       :: Int
+boxBeforeNM =
+  fromIntegral c_crypto_box_beforenmbytes :: Int
+
+-- OneTimeAuth
+onetime    = fromIntegral c_crypto_onetimeauth_bytes    :: Int
+onetimeKey = fromIntegral c_crypto_onetimeauth_keybytes :: Int
+
+-- ScalarMult
+scalarMult = fromIntegral c_crypto_scalarmult_bytes :: Int
+scalarMultScalar =
+  fromIntegral c_crypto_scalarmult_scalarbytes :: Int
+
+-- SecretBox
+secretBoxKey     = fromIntegral c_crypto_secretbox_keybytes     :: Int
+secretBoxNonce   = fromIntegral c_crypto_secretbox_noncebytes   :: Int
+secretBoxZero    = fromIntegral c_crypto_secretbox_zerobytes    :: Int
+secretBoxBoxZero = fromIntegral c_crypto_secretbox_boxzerobytes :: Int
+
+-- Signatures
+sign   = fromIntegral c_crypto_sign_bytes          :: Int
+signPK = fromIntegral c_crypto_sign_publickeybytes :: Int
+signSK = fromIntegral c_crypto_sign_secretkeybytes :: Int
+
+-- src/libsodium/crypto_auth/crypto_auth.c
+foreign import ccall "crypto_auth_bytes"
+  c_crypto_auth_bytes :: CSize
+foreign import ccall "crypto_auth_keybytes"
+  c_crypto_auth_keybytes :: CSize
+
+-- src/libsodium/crypto_box/crypto_box.c
+foreign import ccall "crypto_box_publickeybytes"
+  c_crypto_box_publickeybytes :: CSize
+foreign import ccall "crypto_box_secretkeybytes"
+  c_crypto_box_secretkeybytes :: CSize
+foreign import ccall "crypto_box_beforenmbytes"
+  c_crypto_box_beforenmbytes :: CSize
+foreign import ccall "crypto_box_noncebytes"
+  c_crypto_box_noncebytes :: CSize
+foreign import ccall "crypto_box_zerobytes"
+  c_crypto_box_zerobytes :: CSize
+foreign import ccall "crypto_box_boxzerobytes"
+  c_crypto_box_boxzerobytes :: CSize
+foreign import ccall "crypto_box_macbytes"
+  c_crypto_box_macbytes :: CSize
+                           
+-- src/libsodium/crypto_onetimeauth/crypto_onetimeauth.c
+foreign import ccall "crypto_onetimeauth_bytes"
+  c_crypto_onetimeauth_bytes :: CSize
+foreign import ccall "crypto_onetimeauth_keybytes"
+  c_crypto_onetimeauth_keybytes :: CSize
+
+-- src/libsodium/crypto_scalarmult/crypto_scalarmult.c
+foreign import ccall "crypto_scalarmult_bytes"
+  c_crypto_scalarmult_bytes :: CSize
+foreign import ccall "crypto_scalarmult_scalarbytes"
+  c_crypto_scalarmult_scalarbytes :: CSize
+
+-- src/libsodium/crypto_secretbox/crypto_secretbox.c
+foreign import ccall "crypto_secretbox_keybytes"
+  c_crypto_secretbox_keybytes :: CSize
+foreign import ccall "crypto_secretbox_noncebytes"
+  c_crypto_secretbox_noncebytes :: CSize
+foreign import ccall "crypto_secretbox_zerobytes"
+  c_crypto_secretbox_zerobytes :: CSize
+foreign import ccall "crypto_secretbox_boxzerobytes"
+  c_crypto_secretbox_boxzerobytes :: CSize
+
+-- src/libsodium/crypto_sign/crypto_sign.c
+foreign import ccall "crypto_sign_bytes"
+  c_crypto_sign_bytes :: CSize
+foreign import ccall "crypto_sign_publickeybytes"
+  c_crypto_sign_publickeybytes :: CSize
+foreign import ccall "crypto_sign_secretkeybytes"
+  c_crypto_sign_secretkeybytes :: CSize
+
+-- src/libsodium/crypto_auth/hmacsha256/auth_hmacsha256_api.c
+-- foreign import ccall "crypto_auth_hmacsha256_bytes"
+--   c_crypto_auth_hmacsha256_bytes :: CSize
+-- foreign import ccall "crypto_auth_hmacsha256_keybytes"
+--   c_crypto_auth_hmacsha256_keybytes :: CSize
+
+-- src/libsodium/crypto_auth/hmacsha512256/auth_hmacsha512256_api.c
+-- foreign import ccall "crypto_auth_hmacsha512256_bytes"
+--   c_crypto_auth_hmacsha512256_bytes :: CSize
+-- foreign import ccall "crypto_auth_hmacsha512256_keybytes"
+--   c_crypto_auth_hmacsha512256_keybytes :: CSize
+
+-- src/libsodium/crypto_box/curve25519xsalsa20poly1305/box_curve25519xsalsa20poly1305_api.c
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_publickeybytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_publickeybytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_secretkeybytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_secretkeybytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_beforenmbytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_beforenmbytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_noncebytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_noncebytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_zerobytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_zerobytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_boxzerobytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_boxzerobytes :: CSize
+-- foreign import ccall "crypto_box_curve25519xsalsa20poly1305_macbytes"
+--   c_crypto_box_curve25519xsalsa20poly1305_macbytes :: CSize
+
+-- src/libsodium/crypto_core/hsalsa20/core_hsalsa20_api.c
+-- foreign import ccall "crypto_core_hsalsa20_outputbytes"
+--   c_crypto_core_hsalsa20_outputbytes :: CSize
+-- foreign import ccall "crypto_core_hsalsa20_inputbytes"
+--   c_crypto_core_hsalsa20_inputbytes :: CSize
+-- foreign import ccall "crypto_core_hsalsa20_keybytes"
+--   c_crypto_core_hsalsa20_keybytes :: CSize
+-- foreign import ccall "crypto_core_hsalsa20_constbytes"
+--   c_crypto_core_hsalsa20_constbytes :: CSize
+
+-- src/libsodium/crypto_core/salsa20/core_salsa20_api.c
+-- foreign import ccall "crypto_core_salsa20_outputbytes"
+--   c_crypto_core_salsa20_outputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa20_inputbytes"
+--   c_crypto_core_salsa20_inputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa20_keybytes"
+--   c_crypto_core_salsa20_keybytes :: CSize
+-- foreign import ccall "crypto_core_salsa20_constbytes"
+--   c_crypto_core_salsa20_constbytes :: CSize
+
+-- src/libsodium/crypto_core/salsa2012/core_salsa2012_api.c
+-- foreign import ccall "crypto_core_salsa2012_outputbytes"
+--   c_crypto_core_salsa2012_outputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa2012_inputbytes"
+--   c_crypto_core_salsa2012_inputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa2012_keybytes"
+--   c_crypto_core_salsa2012_keybytes :: CSize
+-- foreign import ccall "crypto_core_salsa2012_constbytes"
+--   c_crypto_core_salsa2012_constbytes :: CSize
+
+-- src/libsodium/crypto_core/salsa208/core_salsa208_api.c
+-- foreign import ccall "crypto_core_salsa208_outputbytes"
+--   c_crypto_core_salsa208_outputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa208_inputbytes"
+--   c_crypto_core_salsa208_inputbytes :: CSize
+-- foreign import ccall "crypto_core_salsa208_keybytes"
+--   c_crypto_core_salsa208_keybytes :: CSize
+-- foreign import ccall "crypto_core_salsa208_constbytes"
+--   c_crypto_core_salsa208_constbytes :: CSize
+
+-- src/libsodium/crypto_generichash/blake2/generichash_blake2_api.c
+-- foreign import ccall "crypto_generichash_blake2b_blockbytes"
+--   c_crypto_generichash_blake2b_blockbytes :: CSize
+
+-- src/libsodium/crypto_generichash/crypto_generichash.c
+-- foreign import ccall "crypto_generichash_bytes"
+--   c_crypto_generichash_bytes :: CSize
+-- foreign import ccall "crypto_generichash_keybytes"
+--   c_crypto_generichash_keybytes :: CSize
+-- foreign import ccall "crypto_generichash_blockbytes"
+--   c_crypto_generichash_blockbytes :: CSize
+
+-- src/libsodium/crypto_hash/sha256/hash_sha256_api.c
+-- foreign import ccall "crypto_hash_sha256_bytes"
+--   c_crypto_hash_sha256_bytes :: CSize
+
+-- src/libsodium/crypto_hash/sha512/hash_sha512_api.c
+-- foreign import ccall "crypto_hash_sha512_bytes"
+--   c_crypto_hash_sha512_bytes :: CSize
+
+-- src/libsodium/crypto_hashblocks/sha256/hashblocks_sha256_api.c
+-- foreign import ccall "crypto_hashblocks_sha256_statebytes"
+--   c_crypto_hashblocks_sha256_statebytes :: CSize
+-- foreign import ccall "crypto_hashblocks_sha256_blockbytes"
+--   c_crypto_hashblocks_sha256_blockbytes :: CSize
+
+-- src/libsodium/crypto_hashblocks/sha512/hashblocks_sha512_api.c
+-- foreign import ccall "crypto_hashblocks_sha512_statebytes"
+--   c_crypto_hashblocks_sha512_statebytes :: CSize
+-- foreign import ccall "crypto_hashblocks_sha512_blockbytes"
+--   c_crypto_hashblocks_sha512_blockbytes :: CSize
+
+-- src/libsodium/crypto_onetimeauth/poly1305/onetimeauth_poly1305_api.c
+-- foreign import ccall "crypto_onetimeauth_poly1305_bytes"
+--   c_crypto_onetimeauth_poly1305_bytes :: CSize
+-- foreign import ccall "crypto_onetimeauth_poly1305_keybytes"
+--   c_crypto_onetimeauth_poly1305_keybytes :: CSize
+
+-- src/libsodium/crypto_secretbox/xsalsa20poly1305/secretbox_xsalsa20poly1305_api.c
+-- foreign import ccall "crypto_secretbox_xsalsa20poly1305_keybytes"
+--   c_crypto_secretbox_xsalsa20poly1305_keybytes :: CSize
+-- foreign import ccall "crypto_secretbox_xsalsa20poly1305_noncebytes"
+--   c_crypto_secretbox_xsalsa20poly1305_noncebytes :: CSize
+-- foreign import ccall "crypto_secretbox_xsalsa20poly1305_zerobytes"
+--   c_crypto_secretbox_xsalsa20poly1305_zerobytes :: CSize
+-- foreign import ccall "crypto_secretbox_xsalsa20poly1305_boxzerobytes"
+--   c_crypto_secretbox_xsalsa20poly1305_boxzerobytes :: CSize
+
+-- src/libsodium/crypto_shorthash/crypto_shorthash.c
+-- foreign import ccall "crypto_shorthash_bytes"
+--   c_crypto_shorthash_bytes :: CSize
+-- foreign import ccall "crypto_shorthash_keybytes"
+--   c_crypto_shorthash_keybytes :: CSize
+
+-- src/libsodium/crypto_shorthash/siphash24/shorthash_siphash24_api.c
+-- foreign import ccall "crypto_shorthash_siphash24_bytes"
+--   c_crypto_shorthash_siphash24_bytes :: CSize
+
+-- src/libsodium/crypto_sign/ed25519/sign_ed25519_api.c
+-- foreign import ccall "crypto_sign_ed25519_bytes"
+--   c_crypto_sign_ed25519_bytes :: CSize
+-- foreign import ccall "crypto_sign_ed25519_publickeybytes"
+--   c_crypto_sign_ed25519_publickeybytes :: CSize
+-- foreign import ccall "crypto_sign_ed25519_secretkeybytes"
+--   c_crypto_sign_ed25519_secretkeybytes :: CSize
+
+-- src/libsodium/crypto_sign/edwards25519sha512batch/sign_edwards25519sha512batch_api.c
+-- foreign import ccall "crypto_sign_edwards25519sha512batch_bytes"
+--   c_crypto_sign_edwards25519sha512batch_bytes :: CSize
+-- foreign import ccall "crypto_sign_edwards25519sha512batch_publickeybytes"
+--   c_crypto_sign_edwards25519sha512batch_publickeybytes :: CSize
+-- foreign import ccall "crypto_sign_edwards25519sha512batch_secretkeybytes"
+--   c_crypto_sign_edwards25519sha512batch_secretkeybytes :: CSize
+
+-- src/libsodium/crypto_stream/aes128ctr/stream_aes128ctr_api.c
+-- foreign import ccall "crypto_stream_aes128ctr_keybytes"
+--   c_crypto_stream_aes128ctr_keybytes :: CSize
+-- foreign import ccall "crypto_stream_aes128ctr_noncebytes"
+--   c_crypto_stream_aes128ctr_noncebytes :: CSize
+-- foreign import ccall "crypto_stream_aes128ctr_beforenmbytes"
+--   c_crypto_stream_aes128ctr_beforenmbytes :: CSize
+
+-- src/libsodium/crypto_stream/aes256estream/stream_aes256estream_api.c
+-- foreign import ccall "crypto_stream_aes256estream_keybytes"
+--   c_crypto_stream_aes256estream_keybytes :: CSize
+-- foreign import ccall "crypto_stream_aes256estream_noncebytes"
+--   c_crypto_stream_aes256estream_noncebytes :: CSize
+-- foreign import ccall "crypto_stream_aes256estream_beforenmbytes"
+--   c_crypto_stream_aes256estream_beforenmbytes :: CSize
+
+-- src/libsodium/crypto_stream/crypto_stream.c
+-- foreign import ccall "crypto_stream_keybytes"
+--   c_crypto_stream_keybytes :: CSize
+-- foreign import ccall "crypto_stream_noncebytes"
+--   c_crypto_stream_noncebytes :: CSize
+
+-- src/libsodium/crypto_stream/salsa2012/stream_salsa2012_api.c
+-- foreign import ccall "crypto_stream_salsa2012_keybytes"
+--   c_crypto_stream_salsa2012_keybytes :: CSize
+-- foreign import ccall "crypto_stream_salsa2012_noncebytes"
+--   c_crypto_stream_salsa2012_noncebytes :: CSize
+
+-- src/libsodium/crypto_stream/salsa208/stream_salsa208_api.c
+-- foreign import ccall "crypto_stream_salsa208_keybytes"
+--   c_crypto_stream_salsa208_keybytes :: CSize
+-- foreign import ccall "crypto_stream_salsa208_noncebytes"
+--   c_crypto_stream_salsa208_noncebytes :: CSize
+
+-- src/libsodium/crypto_stream/xsalsa20/stream_xsalsa20_api.c
+-- foreign import ccall "crypto_stream_xsalsa20_keybytes"
+--   c_crypto_stream_xsalsa20_keybytes :: CSize
+-- foreign import ccall "crypto_stream_xsalsa20_noncebytes"
+--   c_crypto_stream_xsalsa20_noncebytes :: CSize
+
+-- src/libsodium/crypto_verify/16/verify_16_api.c
+-- foreign import ccall "crypto_verify_16_bytes"
+--   c_crypto_verify_16_bytes :: CSize
+
+-- src/libsodium/crypto_verify/32/verify_32_api.c
+-- foreign import ccall "crypto_verify_32_bytes"
+--   c_crypto_verify_32_bytes :: CSize
---
+++ src/cbits/glue.c
@@ -0,0 +1,4 @@
+#include <assert.h>
+#include <sodium.h>
+
+
