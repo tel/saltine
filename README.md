8deba16b540b6bdd8b057c45d488a225f3c0cb4f
Joseph Abrahamson <me@jspha.com>
2013-06-06 03:21:33 -0400

    *tweet*

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Tested with `libsodium-4.1`.
 
 [![Build Status](https://travis-ci.org/tel/saltine.png?branch=master)](https://travis-ci.org/tel/saltine)
+
+# Todos
+
+1. Remove transformers dependency
--- tests/Util.hs
+++ tests/Util.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Util where
 
 import Test.QuickCheck
 
 import Data.Word
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
 import qualified Data.Vector.Storable as V
 
 import Control.Applicative
@@ -20,7 +19,7 @@ newtype Message = Message S.ByteString
                 deriving (Show)
 
 instance Arbitrary Message where
-  arbitrary = Message . S.intercalate " " <$> listOf (oneof [
+  arbitrary = Message . S.intercalate (S8.pack " ") <$> listOf (oneof [
     return "word",
     return "other word"
     ])
