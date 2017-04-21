25e3f4ae9ec5f710260f471542b5b10b2bec3e38
Will Yager <will.yager@gmail.com>
2017-04-21 12:27:34 -0500

    Fix type of boxSeal (#31)
    * Fix type of boxSeal
    
    * Updated tests

--- tests/SealedBoxProperties.hs
+++ tests/SealedBoxProperties.hs
@@ -10,32 +10,38 @@ import           Crypto.Saltine.Core.Box
 import qualified Data.ByteString                      as S
 import           Test.Framework.Providers.QuickCheck2
 import           Test.Framework
+import           Test.QuickCheck.Property (ioProperty)
 
 -- | Ciphertext can be decrypted
-rightInverseProp :: Keypair -> Message -> Bool
-rightInverseProp (sk1, pk1) (Message bs) =
-  Just bs == boxSealOpen pk1 sk1 (boxSeal pk1 bs)
+rightInverseProp :: Keypair -> Message -> IO Bool
+rightInverseProp (sk1, pk1) (Message bs) = do
+  enc <- boxSeal pk1 bs
+  return (Just bs == boxSealOpen pk1 sk1 enc)
 
 -- | Cannot decrypt without the correct secret key
-rightInverseFailureProp1 :: Keypair -> Message -> Bool
-rightInverseFailureProp1 (sk1, pk1) (Message bs) =
-  Nothing == boxSealOpen pk1 (perturb sk1) (boxSeal pk1 bs)
+rightInverseFailureProp1 :: Keypair -> Message -> IO Bool
+rightInverseFailureProp1 (sk1, pk1) (Message bs) = do
+  enc <- boxSeal pk1 bs
+  return (Nothing == boxSealOpen pk1 (perturb sk1) enc)
 
 -- | Cannot decrypt without the correct public key
-rightInverseFailureProp2 :: Keypair -> Message -> Bool
-rightInverseFailureProp2 (sk1, pk1) (Message bs) =
-  Nothing == boxSealOpen (perturb pk1) sk1 (boxSeal pk1 bs)
+rightInverseFailureProp2 :: Keypair -> Message -> IO Bool
+rightInverseFailureProp2 (sk1, pk1) (Message bs) = do
+  enc <- boxSeal pk1 bs
+  return (Nothing == boxSealOpen (perturb pk1) sk1 enc)
 
 -- | Cannot decrypt when not sent to you
-rightInverseFailureProp3 :: Keypair -> Message -> Bool
-rightInverseFailureProp3 (sk1, pk1) (Message bs) =
-  Nothing == boxSealOpen pk1 sk1 (boxSeal (perturb pk1) bs)
+rightInverseFailureProp3 :: Keypair -> Message -> IO Bool
+rightInverseFailureProp3 (sk1, pk1) (Message bs) = do
+  enc <- boxSeal (perturb pk1) bs
+  return (Nothing == boxSealOpen pk1 sk1 enc)
 
 -- | Ciphertext cannot be decrypted (verification failure) if the
 -- ciphertext is perturbed
-rightInverseFailureProp4 :: Keypair -> Message -> Bool
-rightInverseFailureProp4 (sk1, pk1) (Message bs) =
-  Nothing == boxSealOpen pk1 sk1 (S.reverse $ boxSeal pk1 bs)
+rightInverseFailureProp4 :: Keypair -> Message -> IO Bool
+rightInverseFailureProp4 (sk1, pk1) (Message bs) = do
+  enc <- boxSeal pk1 bs
+  return (Nothing == boxSealOpen pk1 sk1 (S.reverse $ enc))
 
 testSealedBox :: Test
 testSealedBox = buildTest $ do
@@ -46,20 +52,20 @@ testSealedBox = buildTest $ do
 
     testGroup "Can decrypt ciphertext using..." [
        testProperty "... public key/secret key"
-       $ rightInverseProp (sk1, pk1)
+       $ ioProperty . rightInverseProp (sk1, pk1)
        ],
 
     testGroup "Fail to verify ciphertext when..." [
       testProperty "... not using proper secret key"
-      $ rightInverseFailureProp1 (sk1, pk1),
+      $ ioProperty . rightInverseFailureProp1 (sk1, pk1),
 
       testProperty "... not using proper public key"
-      $ rightInverseFailureProp2 (sk1, pk1),
+      $ ioProperty . rightInverseFailureProp2 (sk1, pk1),
 
       testProperty "... not actually sent to you"
-      $ rightInverseFailureProp3 (sk1, pk1),
+      $ ioProperty . rightInverseFailureProp3 (sk1, pk1),
 
       testProperty "... ciphertext has been perturbed"
-      $ rightInverseFailureProp4 (sk1, pk1)
+      $ ioProperty . rightInverseFailureProp4 (sk1, pk1)
       ]
     ]
