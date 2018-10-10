3f702ebe67ad5a6bb53852867988bb3796dc8237
Max Amanshauser <max@lambdalifting.org>
2018-10-10 13:55:06 +0200

    try new travis script

--- travis.sh
+++ travis.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+function version_gte() {
+    test "$(printf '%s\n' "$@" | sort -V -r | head -n 1)" == "$1";
+}
+
 rm -rf .cabal-sandbox
 cabal update
-cabal sandbox init
 
-if [ "$CABALVER" != "2.0" ]; then
+if version_gte $CABALVER "2.0"; then
+    cabal new-build --enable-documentation --enable-tests
+    cabal new-test
+    cabal sdist && cabal install --force-reinstalls dist/saltine-*.tar.gz
+else
+    cabal sandbox init
     cabal install --only-dependencies --enable-tests -j4
     cabal build
     cabal haddock
     cabal test --show-details=always
     cabal check
-    cabal sdist && cabal install --force-reinstalls dist/saltine-*.tar.gz
-else
-    cabal new-build --enable-documentation --enable-tests
-    cabal new-test
     cabal sdist && cabal install --force-reinstalls dist/saltine-*.tar.gz
 fi
