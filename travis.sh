abeef62038c989ab9993bf82ff349fe3786b8a8f
Max Amanshauser <max@lambdalifting.org>
2019-08-30 16:12:52 +0200

    Let's try cabal 3.0

--- travis.sh
+++ travis.sh
@@ -8,15 +8,15 @@ rm -rf .cabal-sandbox
 cabal update
 
 if version_gte "$CABALVER" "2.0"; then
-    cabal new-build --enable-documentation --enable-tests
-    cabal new-test --enable-tests || exit 1
-    cabal sdist && cabal install --force-reinstalls dist/saltine-*.tar.gz
+    cabal v2-build --enable-documentation --enable-tests
+    cabal v2-test --enable-tests || exit 1
+    cabal v2-sdist && cabal v2-install --force-reinstalls dist/saltine-*.tar.gz
 else
-    cabal sandbox init
-    cabal install --only-dependencies --enable-tests -j4
-    cabal build
-    cabal haddock
-    cabal test --show-details=always || exit 1
-    cabal check
-    cabal sdist && cabal install --force-reinstalls dist/saltine-*.tar.gz
+    cabal v1-sandbox init
+    cabal v1-install --only-dependencies --enable-tests -j4
+    cabal v1-build
+    cabal v1-haddock
+    cabal v1-test --show-details=always || exit 1
+    cabal v1-check
+    cabal v1-sdist && cabal v1-install --force-reinstalls dist/saltine-*.tar.gz
 fi
