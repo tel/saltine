71676ccfbe88b4f99c63709e339ad91c5b9ef0ef
Max Amanshauser <max@lambdalifting.org>
2021-03-27 13:21:27 +0100

    export bin2hex

--- src/Crypto/Saltine/Core/Utils.hs
+++ src/Crypto/Saltine/Core/Utils.hs
@@ -1,5 +1,6 @@
 module Crypto.Saltine.Core.Utils
-    (Crypto.Saltine.Internal.Util.randomByteString
+    ( Crypto.Saltine.Internal.Util.randomByteString
+    , Crypto.Saltine.Internal.Util.bin2hex
     ) where
 
 import qualified Crypto.Saltine.Internal.Util
