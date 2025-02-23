2317b1d87a8a937719573f7f9f0c9f7837ab39d3
Max Amanshauser <max@lambdalifting.org>
2025-02-22 18:45:35 -0600

    some tests for new code

--- tests/HashProperties.hs
+++ tests/HashProperties.hs
@@ -28,6 +28,9 @@ testHash = buildTest $ do
     testProperty "Hash of empty ByteString is correct"
     $ \(Message bs) -> (bs == S.empty) ==> hash bs == (read hashEmptyBS :: S.ByteString),
 
+    testProperty "SHA256 of empty ByteString is correct"
+    $ \(Message bs) -> (bs == S.empty) ==> sha256 bs == (read sha256EmptyBS :: S.ByteString),
+
     testProperty "No two shorthashes are alike"
     $ \(Message bs1, Message bs2) -> bs1 /= bs2 ==> shorthash shKey bs1 /= shorthash shKey bs2,
 
@@ -37,10 +40,14 @@ testHash = buildTest $ do
     testProperty "No two generic hashes are alike"
     $ \(Message bs1, Message bs2) -> bs1 /= bs2 ==> generichash ghKey bs1 ghOutLen /= generichash ghKey bs2 ghOutLen,
 
+    testProperty "No two SHA256 hashes are alike"
+    $ \(Message bs1, Message bs2) -> bs1 /= bs2 ==> sha256 bs1 /= sha256 bs2,
+
     testProperty "Different keys produce different generichashes"
     $ \(Message bs) -> generichash ghKey bs ghOutLen /= generichash ghKey2 bs ghOutLen
 
     ]
 
   where
-    hashEmptyBS = "\"\207\131\225\&5~\239\184\189\241T(P\214m\128\a\214 \228\ENQ\vW\NAK\220\131\244\169!\211l\233\206G\208\209<]\133\242\176\255\131\CAN\210\135~\236/c\185\&1\189GAz\129\165\&82z\249'\218>\""
+    hashEmptyBS   = "\"\207\131\225\&5~\239\184\189\241T(P\214m\128\a\214 \228\ENQ\vW\NAK\220\131\244\169!\211l\233\206G\208\209<]\133\242\176\255\131\CAN\210\135~\236/c\185\&1\189GAz\129\165\&82z\249'\218>\""
+    sha256EmptyBS = "\"\227\176\196B\152\252\FS\DC4\154\251\244\200\153o\185$'\174A\228d\155\147L\164\149\153\ESCxR\184U\""
--- tests/SignProperties.hs
+++ tests/SignProperties.hs
@@ -5,10 +5,15 @@ module SignProperties (
   ) where
 
 import           Util
+import           Crypto.Saltine.Class
+
 import           Crypto.Saltine.Core.Sign
 import           Crypto.Saltine.Internal.Sign
-
+import qualified Crypto.Saltine.Core.Box              as Box
+import           Crypto.Saltine.Core.ScalarMult       ()
+import           Crypto.Saltine.Internal.ScalarMult
 import qualified Data.ByteString                      as S
+import           Data.Maybe                           (fromJust)
 import           Test.Framework.Providers.QuickCheck2
 import           Test.Framework
 import           Test.QuickCheck
@@ -19,7 +24,9 @@ testSign = buildTest $ do
   let sk1 = secretKey kp1
   let pk1 = publicKey kp1
   kp2 <- newKeypair
+  let sk2 = secretKey kp2
   let pk2 = publicKey kp2
+  n <- Box.newNonce
 
   return $ testGroup "...Internal.Sign" [
 
@@ -38,6 +45,15 @@ testSign = buildTest $ do
 
     testProperty "Rejects message with mismatched key w/ detached signature"
     $ \(Message bs) -> not (S.null bs) ==>
-                         not (signVerifyDetached pk2 (signDetached sk1 bs) bs)
+                         not (signVerifyDetached pk2 (signDetached sk1 bs) bs),
 
+    testProperty "Ed25515 -> Curve25519 conversion produces valid keypairs"
+    $ \(Message bs) ->
+        let p1 = fromJust . decode . unGE . fromJust $ signPublicKeyToScalarMult pk1
+            s1 = fromJust . decode . unSc . fromJust $ signSecretKeyToScalarMult sk1
+
+            p2 = fromJust . decode . unGE . fromJust $ signPublicKeyToScalarMult pk2
+            s2 = fromJust . decode . unSc . fromJust $ signSecretKeyToScalarMult sk2
+
+        in  (Box.boxOpen p2 s1 n (Box.box p1 s2 n bs)) == Just bs
     ]
